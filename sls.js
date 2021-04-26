6f718ef4b944b93d4fd42481026ecdbfafb39817
郭凯南 <835984026@qq.com>
2021-04-26 09:13:35 +0800

    sls.js

--- sls.js
+++ sls.js
@@ -4,11 +4,11 @@
  * @Author: Guo Kainan
  * @Date: 2021-04-26 08:53:28
  * @LastEditors: Guo Kainan
- * @LastEditTime: 2021-04-26 08:54:32
+ * @LastEditTime: 2021-04-26 09:13:29
  */
 
 // Serverless入口文件
-const createApp = require('./app')
+const createApp = require('./server/app')
 
 const app = createApp()
 
