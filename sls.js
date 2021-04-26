d438348a8272c699e1ee26b1fa4177ff6ada38dd
郭凯南 <835984026@qq.com>
2021-04-26 11:02:18 +0800

    serverless dist

--- sls.js
+++ sls.js
@@ -4,12 +4,10 @@
  * @Author: Guo Kainan
  * @Date: 2021-04-26 08:53:28
  * @LastEditors: Guo Kainan
- * @LastEditTime: 2021-04-26 09:13:29
+ * @LastEditTime: 2021-04-26 11:02:03
  */
 
 // Serverless入口文件
 const createApp = require('./server/app')
 
-const app = createApp()
-
-module.exports = app
+module.exports = createApp
